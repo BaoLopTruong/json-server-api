173835d26ff98270e1d70e5ab44b7f957413b969
BaoLopTruong <ngocbaole1999@gmail.com>
2022-06-13 08:09:04 +0700

    update database

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 
 // Add custom routes before JSON Server router
-server.get('/api', (req, res) => {
+server.get('/echo', (req, res) => {
   res.jsonp(req.query)
 })
 
